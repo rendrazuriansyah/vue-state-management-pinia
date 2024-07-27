ee75c9ad2c4aef79b5b189cc8f7e4345fd1673cc
twingqywingqy <rendra.zurd04@gmail.com>
2024-07-27 23:55:32 +0700

    refactor: switch from ref to reactive for color management

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,59 @@
 <script setup>
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 
-const red = ref(0);
-const green = ref(0);
-const blue = ref(0);
+// penggunaan ref yang direkomendasikan
+// const red = ref(0);
+// const green = ref(0);
+// const blue = ref(0);
+
+// refuntuk membuat nilai reaktif untuk tipe data primitif
+// Mengakses nilai:
+// console.log(color.value.red);
+// Mengubah nilai:
+// color.value.red = 255;
+// inisiasi objek dalam ref:
+// const color = ref({
+// 	red: 0,
+// 	green: 0,
+// 	blue: 0,
+// });
+
+// reactive untuk menangani reaktivitas pada objek dan array
+// Mengakses nilai:
+// console.log(color.red);
+// Mengubah nilai:
+// color.red = 255
+// inisiasi objek dalam reactive:
+const color = reactive({
+	red: 0,
+	green: 0,
+	blue: 0,
+});
 </script>
 
 <template>
-	<main :style="{ backgroundColor: `rgb(${red}, ${green}, ${blue})` }">
+	<main
+		:style="{
+			backgroundColor: `rgb(${color.red}, ${color.green}, ${color.blue})`,
+		}"
+	>
 		<div>
 			<input
 				type="number"
 				placeholder="red"
-				v-model="red"
+				v-model="color.red"
 				id=""
 			/>
 			<input
 				type="number"
 				placeholder="green"
-				v-model="green"
+				v-model="color.green"
 				id=""
 			/>
 			<input
 				type="number"
 				placeholder="blue"
-				v-model="blue"
+				v-model="color.blue"
 				id=""
 			/>
 		</div>
